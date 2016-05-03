fca96f382a93489e467b1ec6c65d944cb2fe39e0
Martin Meredith <martin@justsayplease.co.uk>
2016-05-03 17:36:28 +0100

    Cleanup of Permissions

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,18 +1,30 @@
 chrome.browserAction.onClicked.addListener(function (activeTab) {
-    chrome.cookies.getAll({url: activeTab.url}, function (cookies) {
-        $.each(cookies, function (index, cookie) {
-            chrome.cookies.remove({
-                    url: activeTab.url,
-                    name: cookie.name
-                }
-            );
-        });
-    });
+    chrome.permissions.request({
+            permissions: ['cookies'],
+            origins: [activeTab.url]
+        },
+        function (granted) {
+            var message = "Omnomnomnom";
+            
+            if (granted) {
+                chrome.cookies.getAll({url: activeTab.url}, function (cookies) {
+                    $.each(cookies, function (index, cookie) {
+                        chrome.cookies.remove({
+                                url: activeTab.url,
+                                name: cookie.name
+                            }
+                        );
+                    });
+                });
+            } else {
+                message = "Cookie Monster is sad that there are no cookies";
+            }
 
-    chrome.notifications.create(options = {
-        type: "basic",
-        iconUrl: "images/icon128.png",
-        message: "Omnomnomnom",
-        title: "Cookie Monster says"
-    });
+            chrome.notifications.create(options = {
+                type: "basic",
+                iconUrl: "images/icon128.png",
+                message: message,
+                title: "Cookie Monster says"
+            });
+        });
 });
